42108ea9fc2b675b23ad2b265481711607250e03
SC-IDO <ido@sitecore.net>
2018-06-21 14:28:09 +0300

    Reworked existing patch for XP 9.0.1

---
+++ src/Sitecore.Support.110796/ContentSearch/SolrProvider/SolrSearchFieldConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace Sitecore.Support.ContentSearch.SolrProvider
+{
+  [Serializable]
+  public class SolrSearchFieldConfiguration : Sitecore.ContentSearch.SolrProvider.SolrSearchFieldConfiguration
+  {
+    public SolrSearchFieldConfiguration() : base() { }
+
+    public SolrSearchFieldConfiguration(SolrSearchFieldConfiguration configuration) : base(configuration) { }
+
+    public SolrSearchFieldConfiguration(string typeName, IDictionary<string, string> attributes, XmlNode configNode) : base(typeName, attributes, configNode) { }
+
+    public override object FormatForReading(object valueFromIndex)
+    {
+      if (valueFromIndex == null) return null;
+
+      if (valueFromIndex is IEnumerable)
+      {
+        foreach (string str in (IEnumerable)valueFromIndex)
+        {
+          if (str == this.NullValue)
+          {
+            return null;
+          }
+        }
+      }
+
+      var stringValue = valueFromIndex as string;
+      if (stringValue != null)
+      {
+        if (stringValue == this.NullValue) return null;
+
+        if (stringValue == string.Empty || stringValue == this.EmptyString) return string.Empty;
+      }
+
+      return valueFromIndex;
+    }
+  }
+}
